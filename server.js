8c84d95d644d820c51c8ad11610c785b9d207790
JohanSora <johan.sora@mymarketlogic.com>
2022-10-07 08:55:34 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { connect } = require("./src/db");
+const userRouter = require("./src/routes/user");
+
+const port = 8080;
+const app = express();
+connect();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("Estamos al aire");
+});
