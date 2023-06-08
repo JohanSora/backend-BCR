d1823b893230b4bd0f3939634179d459921adb84
mlguatemala <carlos.vargas@mymarketlogic.com>
2023-06-08 00:47:25 -0600

    Update reporter.service.js

--- src/services/reporters/reporter.service.js
+++ src/services/reporters/reporter.service.js
@@ -52,14 +52,14 @@ class ReporterService {
     }
   }
 
-  async getPointsAssignByUser(email) {
-    let email = email;
+  async getPointsAssignByUser(emailuser) {
+    let email = emailuser;
     const query = `SELECT s.sale_date, s.quarter_id, s.week_file, s.employ_assigned_id, u.email, s.invoice_number,
     s.product_id, p.description,s.sale_type, s.sales_amount, s.assigned_points
     FROM sales s
     JOIN users u ON s.employ_assigned_id = u.id
     JOIN products p ON s.product_id = p.id
-    WHERE s.error_id IS null and sales_amount > 0 AND u.email = '${types}'
+    WHERE s.error_id IS null and sales_amount > 0 AND u.email = '${emailuser}'
     ORDER BY s.sale_date DESC;`;
     try {
 
